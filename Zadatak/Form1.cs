9a11d555a4d6f058dfa01a0e989281dd844f92e2
AntonijaPandzic <antonija.pandzic@skole.hr>
2021-05-02 21:10:47 +0200

    Update Form1.cs

--- Zadatak/Form1.cs
+++ Zadatak/Form1.cs
@@ -31,6 +31,27 @@ namespace WindowsFormsApp2
             listBox1.Items.Add("Highest number is: " + highestNumber);
 
             double averageValue = numberList.Average();listBox1.Items.Add("Average of all values is: " + averageValue); 
+
+            bool doesContain = numberList.Contains(3);listBox1.Items.Add("Contains the number 3: " + doesContain);
+            int elementValue = numberList.ElementAt(1); listBox1.Items.Add("The element at array position 1 is: " + elementValue);
+
+            int firstElement = numberList.First(); 
+            listBox1.Items.Add("First Array Value = " + firstElement);   
+            int lastElement = numberList.Last();     
+            listBox1.Items.Add("Last Array Value = " + lastElement);
+
+            listbox:listBox1.Items.Add("==================================");listBox1.Items.Add("ARRAY ITEMS: 1, 1, 2, 2, 3, 4, 5, 5");    
+            listBox1.Items.Add("REMOVE DUPLICATES FROM ARRAY"); 
+            listBox1.Items.Add("==================================");
+
+             int[] aryNums = new int[8] { 1, 1, 2, 2, 3, 4, 5, 5 };
+             var distinctNums = aryNums.Distinct(); 
+
+             foreach (var num in distinctNums)      
+                   {    
+                      listBox1.Items.Add(num.ToString());        
+                                        }
         }
+
     }
 }
