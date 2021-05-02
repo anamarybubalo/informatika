55f3d0e2d1367d2293656027f320a911cdab30e3
AntonijaPandzic <antonija.pandzic@skole.hr>
2021-05-02 20:57:54 +0200

    Update Form1.cs

--- Zadatak/Form1.cs
+++ Zadatak/Form1.cs
@@ -23,6 +23,11 @@ namespace WindowsFormsApp2
             listBox1.Items.Add("ARRAY ITEMS: 1, 2, 3, 4, 5"); 
             listBox1.Items.Add("========================");
             int listTotal = numberList.Sum();
+
+            int lowestNumber = numberList.Min();      
+            listBox1.Items.Add("Lowest number is: " + lowestNumber);       
+            int highestNumber = numberList.Max();         
+            listBox1.Items.Add("Highest number is: " + highestNumber);
         }
     }
 }
